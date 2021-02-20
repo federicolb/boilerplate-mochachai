5ca2269677eb61ca0e94a0d65dbc6e3033dd5d44
root <fedelibar@gmail.com>
2021-02-20 08:47:28 -0300

    unit-test-ex5-a

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -35,7 +35,7 @@ suite('Unit Tests', function () {
     test('#equal, #notEqual', function () {
       assert.equal(12, '12', 'numbers are coerced into strings with == ');
       assert.notEqual({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.notEqual(6 * '2', '12', 'no more hints...');
+      assert.equal(6 * '2', '12', 'no more hints...');
       assert.notEqual(6 + '2', '12', 'type your error message if you want');
     });
     // #6
